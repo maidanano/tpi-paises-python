8a31d5f62f2720434e362546625bf74bce7da404
magasal17 <marianogallosaluzzi@gmail.com>
2026-06-13 01:21:42 -0300

    Update modulo_visualizacion.py

--- modulos/modulo_visualizacion.py
+++ modulos/modulo_visualizacion.py
@@ -1,1 +1,40 @@
+# ---------------- FUNCIONES DE VISUALIZACIÓN ----------------
 
+def mostrar_menu():
+    """
+    Muestra el menú principal del sistema.
+    """
+    print("\n---------- MENÚ PRINCIPAL ----------")
+    print("1. Mostrar todos los países")
+    print("2. Agregar un país")
+    print("3. Actualizar población y superficie de un país")
+    print("4. Buscar país por nombre")
+    print("5. Filtrar países por continente")
+    print("6. Filtrar países por rango de población")
+    print("7. Filtrar países por rango de superficie")
+    print("8. Ordenar países")
+    print("9. Mostrar estadísticas")
+    print("0. Salir")
+
+
+def mostrar_pais(pais):
+    
+    #Muestra los datos de un país.
+    
+    print(f"Nombre: {pais['nombre']}")
+    print(f"Población: {pais['poblacion']}")
+    print(f"Superficie: {pais['superficie']} km²")
+    print(f"Continente: {pais['continente']}")
+    print("-----------------------------------")
+
+
+def mostrar_paises(paises):
+    
+    #Muestra una lista de países.
+    
+    if len(paises) == 0:
+        print("No hay países para mostrar.")
+    else:
+        print("\n---------- LISTADO DE PAÍSES ----------")
+        for pais in paises:
+            mostrar_pais(pais)
